8d5531b13d821e0a1710a6f1cc6421f191cc3993
sahilnikalje <sahilnik88@gmail.com>
2026-02-08 16:15:28 +0530

    working auth completed

---
+++ Backend/server.js
@@ -0,0 +1,34 @@
+const express=require('express')
+const cors=require('cors')
+const dotenv=require('dotenv').config()
+const cookieParser=require('cookie-parser')
+const connectDb=require('./config/db')
+const authRouter=require('./routes/authRoutes')
+
+const app=express()
+
+const PORT=process.env.PORT
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({credentials:true}))
+
+
+app.get('/', (req,res)=>{
+    res.send("Hello there")
+})
+app.use('/api/auth', authRouter)
+
+const startServer=async ()=>{
+    try{
+        await connectDb()
+        app.listen(PORT,()=>{
+          console.log(`Server running on Port ${PORT}`)
+        })
+    }
+    catch(err){
+        console.log(err.message)
+        process.exit(1)
+    }
+}
+startServer()
