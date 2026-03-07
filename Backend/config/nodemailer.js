5bd5d14d6174a36e4a801c534d7c4f24aa86fa1b
sahilnikalje <sahilnik88@gmail.com>
2026-03-07 17:39:06 +0530

    nodemailer issue resolved

--- Backend/config/nodemailer.js
+++ Backend/config/nodemailer.js
@@ -2,8 +2,8 @@ const nodemailer=require('nodemailer')
 
 const transporter=nodemailer.createTransport({
     host:"smtp-relay.brevo.com",
-    port:587,
-    secure:false,
+    port:465,
+    secure:true,
     auth:{
         user:process.env.SMTP_USER,
         pass:process.env.SMTP_PASSWORD,
