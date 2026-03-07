ff946c3b82c51d2a6e667fca221731f90d41bcc8
sahilnikalje <sahilnik88@gmail.com>
2026-03-07 19:39:38 +0530

    remove nodemailer transporter verification code

--- Backend/server.js
+++ Backend/server.js
@@ -8,13 +8,6 @@ const userRouter=require('./routes/userRoutes')
 
 const app=express()
 
-const transporter = require('./config/nodemailer')
-
-transporter.verify((error, success) => {
-    if (error) console.log("EMAIL TRANSPORTER ERROR:", error)
-    else console.log("Email transporter ready")
-})
-
 app.set("trust proxy", 1)
 const PORT=process.env.PORT
 
